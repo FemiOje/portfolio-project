6b1506f1c3e7af15c24b35c4d5c1fee572f9c6b4
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-10-17 00:53:23 +0100

    do-over hehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import './App.css'
-import Navbar from './components/Navbar.jsx'
-import Hero from './components/Hero.jsx'
-import AboutMe from './components/AboutMe.jsx'
-import ProjectList from './components/ProjectList.jsx'
-import ContactMe from './components/ContactMe.jsx'
-import Footer from './components/Footer.jsx'
-
 
 function App() {
 
   return (
     <div className="App">
-      <div className="navbar-and-hero">
-        <Navbar />
-        <Hero />
-      </div>
-
-      <AboutMe />
-      <ProjectList />
-      <ContactMe />
-      <Footer />
+      <p className='text-center bg-blue-500 text-white p-4'>Hi! I&apos;m Jinius</p>
     </div>
   )
 }
